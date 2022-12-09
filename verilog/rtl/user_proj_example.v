1f4c45e2fc69bfe5bcac958f008543e0d240c22c
Yash Kothari <69398841+yashkthr@users.noreply.github.com>
2022-12-09 16:44:14 +0530

    Update user_proj_example.v

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -121,45 +121,57 @@ module user_proj_example #(
 
 endmodule
 
-module counter #(
-    parameter BITS = 32
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output ready,
-    output [BITS-1:0] rdata,
-    output [BITS-1:0] count
-);
-    reg ready;
-    reg [BITS-1:0] count;
-    reg [BITS-1:0] rdata;
-
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 0;
-            ready <= 0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-                if (wstrb[2]) count[23:16] <= wdata[23:16];
-                if (wstrb[3]) count[31:24] <= wdata[31:24];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
+module iiitb_lifo( dataIn, dataOut, RW, EN,
+                   Rst,
+                   EMPTY,
+                   FULL,
+                   clk
+                  );
+input [3:0]  dataIn;
+input        RW,
+             EN,
+             Rst,
+             clk;
+     
+output  reg EMPTY,
+            FULL;    
+output reg [3:0] dataOut;
+reg [3:0] stack_mem[0:3];
+reg  [2:0] SP;
+integer i;
+  always @ (posedge clk)
+begin
+ if (EN==0);
+ else begin
+    if (Rst==1) begin
+      SP       = 3'd4;
+      EMPTY    = SP[2];
+      dataOut  = 4'h0;
+      for (i=0;i<4;i=i+1) begin
+        stack_mem[i]= 0;
+      end
+     end
+  else if (Rst==0) begin
+    FULL = SP? 0:1;
+    EMPTY  = SP[2];
+    dataOut = 4'hx;
+    if (FULL == 1'b0 && RW == 1'b0) begin
+       SP            = SP-1'b1;
+       FULL          = SP? 0:1;
+       EMPTY         = SP[2];
+       stack_mem[SP] = dataIn;
+     end
+   else if (EMPTY == 1'b0 && RW == 1'b1) begin
+       dataOut  = stack_mem[SP];
+       stack_mem[SP] = 0;
+       SP  = SP+1;
+       FULL  = SP? 0:1;
+       EMPTY  = SP[2];
     end
-
+   else;
+  end
+  else;
+ end
+end
 endmodule
 `default_nettype wire
